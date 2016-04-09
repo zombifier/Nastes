4c3d69cf8645edc66e102320042cd517a6e2481e
Steven Huang <samhuang25@gmail.com>
2016-04-09 15:11:49 -0400

    ln

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
     	System.out.println("Hello");
     	System.out.println("hi");
-    	System.out.printIn("Sam");
+    	System.out.println("Sam");
     }
 }
