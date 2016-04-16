ce7b2268127116013c2ca84b3b2eb617483fb717
Steven Huang <samhuang25@gmail.com>
2016-04-15 20:58:22 -0400

    SquareView

---
+++ src/view/SquareView.java
@@ -0,0 +1,18 @@
+package view;
+
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import java.awt.Color;
+
+public class SquareView extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public SquareView() {
+		setBackground(Color.GRAY);
+		FlowLayout flowLayout = (FlowLayout) getLayout();
+
+	}
+
+}
