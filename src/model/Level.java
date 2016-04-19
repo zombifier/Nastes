85789da9fb7e6e98fb8d3f990a6e0809f14ac2a4
Chaiwat Ekkaewnumchai <cekkaewnumchai@wpi.edu>
2016-04-19 19:22:37 -0400

    Level updated

--- src/model/Level.java
+++ src/model/Level.java
@@ -4,6 +4,10 @@ public class Level {
 	Board board;
 	Bullpen bullpen;
 	Piece pieceBeingDragged;
+	int starEarned;
+	boolean isUnlocked;
+	int levelNum;
+	Hint hint;
 	
 	public Level(){
 		board=new Board();
@@ -17,5 +21,13 @@ public class Level {
 		pieceBeingDragged=null;
 	}
 	
+	public void holdPiece(Piece p){
+		pieceBeingDragged=p;
+	}
 	
+	public Piece releasePiece(){
+		Piece p=pieceBeingDragged;
+		pieceBeingDragged=null;
+		return p;
+	}
 }
