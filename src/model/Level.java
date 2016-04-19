e15624927056e8d704ea610af3f6b87d99717b7e
Chaiwat Ekkaewnumchai <cekkaewnumchai@wpi.edu>
2016-04-19 18:51:04 -0400

    Game and Level constructors added

--- src/model/Level.java
+++ src/model/Level.java
@@ -4,4 +4,18 @@ public class Level {
 	Board board;
 	Bullpen bullpen;
 	Piece pieceBeingDragged;
+	
+	public Level(){
+		board=new Board();
+		bullpen=new Bullpen();
+		pieceBeingDragged=null;
+	}
+
+	public Level(Board b,Bullpen p){
+		board=b;
+		bullpen=p;
+		pieceBeingDragged=null;
+	}
+	
+	
 }
