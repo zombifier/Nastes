7ad8c20f6ad651ca93324ea46a18f33d0fb0d63e
Duong Nguyen <nguyendu@proctornet.com>
2016-04-22 20:28:04 -0400

    Bullpen Fix

--- src/view/BullpenView.java
+++ src/view/BullpenView.java
@@ -24,12 +24,8 @@ public class BullpenView extends JPanel {
 		
 		setBackground(new Color(221, 160, 221));
 		setPreferredSize(new Dimension(430,600));
-<<<<<<< HEAD
 		setLayout(null);
-=======
->>>>>>> branch 'master' of https://github.com/zombifier/Nastes.git
 		
-<<<<<<< HEAD
 		PieceView pieceView = new PieceView(new Piece(1,3));
 		pieceView.setBounds(10, 10, 270, 270);
 		add(pieceView);
@@ -40,50 +36,6 @@ public class BullpenView extends JPanel {
 //		add(pieceView_1);
 //		PieceView pieceView_2 = new PieceView(new Piece(3,2));
 //		add()
-=======
-		PieceView pieceView = new PieceView(null);
->>>>>>> branch 'master' of https://github.com/zombifier/Nastes.git
-		
-<<<<<<< HEAD
-=======
-		PieceView pieceView_1 = new PieceView(null);
-		
-		PieceView pieceView_2 = new PieceView(null);
-		
-		JLabel lblThisIsBullpenview = new JLabel("This is BullPenView");
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(pieceView_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(pieceView_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(180)
-							.addComponent(lblThisIsBullpenview))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(pieceView, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(10)
-					.addComponent(lblThisIsBullpenview)
-					.addGap(18)
-					.addComponent(pieceView, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(32)
-					.addComponent(pieceView_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
-					.addComponent(pieceView_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(22))
-		);
-		setLayout(groupLayout);
->>>>>>> branch 'master' of https://github.com/zombifier/Nastes.git
 
 	}
 }
