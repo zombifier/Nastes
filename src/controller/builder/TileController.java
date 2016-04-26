50c367e24a8b093445f46019d7632518bd2e2b22
jbardonjr <jbardonjr@wpi.edu>
2016-04-26 18:13:00 -0400

    Future-proofing the mousePresssed event

--- src/controller/builder/TileController.java
+++ src/controller/builder/TileController.java
@@ -17,9 +17,23 @@ public class TileController extends MouseAdapter {
 	@Override
 	public void mousePressed(MouseEvent ae){
 		if(tile != null && tileView != null){
-			tile.convertValid();
-			tileView.redraw();
-			System.out.println("Working");
+			//Check which mouse button was pressed
+		    switch(ae.getModifiers()) {
+		    	//Left click
+		      	case InputEvent.BUTTON1_MASK: {
+		      		tile.convertValid();
+					tileView.redraw();
+					System.out.println("Tile state toggled."); 
+		        	break;
+		      	}
+		      	//Right click
+		    	case InputEvent.BUTTON3_MASK: {
+		    		//Insert code for release tile checking
+		    		//Insert code for being able to update the color/number, if release
+		    	  	System.out.println("That's the RIGHT button");     
+		    		break;
+		      	}
+		    }
 		}
 	}
 }
