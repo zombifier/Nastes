c512e0b8f021ce04535db472ea014b2935927ca2
jbardonjr <jbardonjr@wpi.edu>
2016-04-27 12:44:55 -0400

    Further comments on controller classes.

--- src/controller/ReturnSelectLevel.java
+++ src/controller/ReturnSelectLevel.java
@@ -7,20 +7,31 @@ import model.*;
 import view.PlayerApplication;
 import view.PlayerSelector;
 
+/**
+ * ReturnSelectLevel class.
+ * Handles taking the player back to the level selection menu.
+ * @author Team Nastes
+ */
 public class ReturnSelectLevel implements ActionListener {
 	PlayerApplication app;
 	Game game;
 
+	/**
+	 * ReturnSelectLevel constructor. Initializes the values of app and game.
+	 * @param game
+	 * @param app
+	 */
 	public ReturnSelectLevel(Game game, PlayerApplication app){
 		this.app = app;
 		this.game = game;
 	}
 	
+	/**
+	 * No need to remember current level progress.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		app.updatePanel(new PlayerSelector(game, app));
-		System.out.println("Clicked");
+		System.out.println("Clicked on the select level button.");
 	}
-	
-
 }
--- src/controller/SelectLevel.java
+++ src/controller/SelectLevel.java
@@ -6,21 +6,35 @@ import java.awt.event.ActionListener;
 import model.*;
 import view.*;
 
+/**
+ * SelectLevel class.
+ * Allows the player to select a level from the level selector.
+ * @author Team Nastes
+ */
 public class SelectLevel implements ActionListener {
 	PlayerApplication app;
 	Level level;
 	Game game;
 
+	/**
+	 * Select Level constructor. Initializes app, level and game.
+	 * @param level
+	 * @param game
+	 * @param app
+	 */
 	public SelectLevel(Level level, Game game, PlayerApplication app){
 		this.app = app;
 		this.level = level;
 		this.game = game;
 	}
 	
+	/**
+	 * Opens the selected level.
+	 * Note that level must be passed in.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		app.updatePanel(new PlayerGame(level, game, app));
 		System.out.println("Clicked");
 	}
-	
 }
